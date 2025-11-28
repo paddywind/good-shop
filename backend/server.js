fb319ae4a526b631957d9af233408a5a5254719e
Nabil Mahmud <nabilmahmud2025@gmail.com>
2025-11-28 15:44:24 +0600

    add cors env

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Allow cross-origin requests from Next.js frontend
+app.use(cors({ origin: process.env.NEXT_PUBLIC_API_URL })); // Allow cross-origin requests from Next.js frontend
 app.use(express.json()); // Body parser for JSON data
 
 // 📦 MongoDB Connection
